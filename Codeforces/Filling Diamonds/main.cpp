7aeffa7a7f88c10dcc4b1aa59f894037b4fedc29
SebastianHall69 <sebastianhall13@gmail.com>
2020-08-05 02:17:19 -0700

    Easy problems for this weeks ACM contest

---
+++ Codeforces/Filling Diamonds/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Sebastian Hall
+ * Created on August 5, 2020, 1:54 AM
+ * Purpose: https://codeforces.com/contest/1339/problem/A
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t = 0, n = 0;
+    
+    cin>>t;
+    
+    for(int i = 0; i < t; ++i) {
+        cin>>n;
+        cout<<n<<endl;
+    }
+    //A Sebastian Production
+    return 0;
+}
---
+++ Codeforces/Magical Sticks/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Sebastian Hall
+ * Created on August 5, 2020, 1:56 AM
+ * Purpose: https://codeforces.com/contest/1371/problem/A
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t = 0, n = 0;
+
+    cin>>t;
+    
+    for(int i = 0; i < t; ++i) {
+        cin>>n;
+        cout<<((n + 1) / 2)<<endl;
+    }
+
+    //A Sebastian Production
+    return 0;
+}
